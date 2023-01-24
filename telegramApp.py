2bbeaa54f1a62cf1810a58838dfd3951c9ba23e6
Marc Schneider <marc@shnei.de>
2023-01-24 16:56:21 +0100

    cleaned up telegramApp.py

--- telegramApp.py
+++ telegramApp.py
@@ -1,14 +1,11 @@
-from typing import Optional
 
-from telegram import Update, InputMediaDocument, InputMediaPhoto
-from typing import DefaultDict, Optional, Set
+from typing import DefaultDict, Optional
 from collections import defaultdict
-from telegram.constants import ParseMode
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, CallbackContext, \
-    ExtBot, Application, TypeHandler, CallbackQueryHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CommandHandler, ContextTypes, CallbackContext, \
+    ExtBot, Application, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, InputMediaPhoto
 
-from game import Challenge, Player, Game
+from game import Game
 
 
 class ChatData:
@@ -20,40 +17,38 @@ class ChatData:
         self.challenges = {}
 
 
-# The [ExtBot, dict, ChatData, dict] is for type checkers like mypy
-class CustomContext(CallbackContext[ExtBot, dict, ChatData, dict]):
+class CustomContext(CallbackContext):
     """Custom class for context."""
 
-    def __init__(self, application: Application, chat_id: int = None, user_id: int = None):
+    def __init__(self, application, chat_id = None, user_id = None):
         super().__init__(application=application, chat_id=chat_id, user_id=user_id)
         self._message_id: Optional[int] = None
 
     @property
     def player(self):
-        """Custom shortcut to access a value stored in the bot_data dict"""
+        """Custom shortcut to access the player from the context."""
         global config
         game = Game(config)
         player = game.get_player(self._user_id)
         return player
 
 
-
     @property
-    def challenge(self) -> Optional[Challenge]:
-        """Access the number of clicks for the message this context object was built for."""
+    def challenge(self):
+        """Access the challenge for the message this context object was built for."""
         if self._message_id:
             return self.chat_data.challenges[self._message_id]
         return None
 
     @challenge.setter
-    def challenge(self, value: Challenge) -> None:
+    def challenge(self, value):
         """Allow to change the challenge"""
         if not self._message_id:
             raise RuntimeError("There is no message associated with this context object.")
         self.chat_data.challenges[self._message_id] = value
 
     @classmethod
-    def from_update(cls, update: object, application: "Application") -> "CustomContext":
+    def from_update(cls, update, application):
         """Override from_update to set _message_id."""
         # Make sure to call super()
         context = super().from_update(update, application)
@@ -63,17 +58,17 @@ class CustomContext(CallbackContext[ExtBot, dict, ChatData, dict]):
         return context
 
 
-def text_from_challenge(challenge: Challenge,index):
+def text_from_challenge(challenge,index):
     emoji = "✅" if challenge.picked[index] == 1 else ("❌" if challenge.picked[index] == -1 else " ")
     text=emoji + " " + challenge.options[index].name + " " + emoji
     return text
 
-def button_from_challenge(challenge: Challenge, index):
+def button_from_challenge(challenge, index):
     but = InlineKeyboardButton(text=text_from_challenge(challenge,index), callback_data=index)
     return but
 
 
-def keyboard_from_challenge(challenge: Challenge):
+def keyboard_from_challenge(challenge):
     keyboard = [
         [
             button_from_challenge(challenge, 0),
@@ -87,7 +82,7 @@ def keyboard_from_challenge(challenge: Challenge):
     reply_markup = InlineKeyboardMarkup(keyboard)
     return reply_markup
 
-async def start(update: Update, context: CustomContext) -> None:
+async def start(update, context):
     """Display a message with a button."""
     player = context.player
     challenge = player.generate_challenge()
@@ -99,8 +94,8 @@ async def start(update: Update, context: CustomContext) -> None:
     context.chat_data.challenges[ret.message_id] = challenge
 
 
-async def answer_click(update: Update, context: CustomContext) -> None:
-    """Rect to click."""
+async def answer_click(update, context):
+    """React to click."""
     await update.callback_query.answer()
     choosen = int(update.callback_query.data)
     if context.challenge.picked[choosen] == 0:
@@ -111,16 +106,6 @@ async def answer_click(update: Update, context: CustomContext) -> None:
                     caption="Score: " + str(context.player.score()) + " 🚀",),
             reply_markup=keyboard_from_challenge(context.challenge)
         )
-
-
-
-async def print_users(update: Update, context: CustomContext) -> None:
-    """Show which users have been using this bot."""
-    await update.message.reply_text(
-        "The following user IDs have used this bot: "
-        f'{", ".join(map(str, context.bot_user_ids))}'
-    )
-
 
 
 
